9e7a35230ebbe47f1be23dd4543a3658ca98b2da
Pruthviraj Shinde <pruthvishinde17@gmail.com>
2026-07-27 21:49:23 +0530

    Refactor fertilizer dashboard JavaScript code

--- fertilizer-dashboard.js
+++ fertilizer-dashboard.js
@@ -102,7 +102,7 @@ form.addEventListener("submit", async (e) => {
   try {
     setLoading(true);
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("***********", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
